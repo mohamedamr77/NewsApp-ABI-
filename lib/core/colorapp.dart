cf619b436165044b5d5a9252edf0c2a0b569e860
mohamedamr77 <moamr947@gmail.com>
2024-04-14 01:56:55 +0200

    Your commit message

--- lib/core/colorapp.dart
+++ lib/core/colorapp.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
 class ColorApp{
-
+  static const Color whiteColor=Colors.white;
+  //second branch
 }
--- lib/core/imageapp.dart
+++ lib/core/imageapp.dart
@@ -1,3 +1,3 @@
 class ImageApp{
-
+//sdfsdfs
 }
