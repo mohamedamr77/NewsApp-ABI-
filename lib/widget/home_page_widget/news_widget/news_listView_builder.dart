d831c014a6aed803261d26cf4215f413fb743afa
mohamedamr77 <moamr947@gmail.com>
2024-05-16 19:32:05 +0300

    @

--- lib/widget/home_page_widget/news_widget/news_listView_builder.dart
+++ lib/widget/home_page_widget/news_widget/news_listView_builder.dart
@@ -12,7 +12,7 @@ class NewsListViewBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return FutureBuilder(
+    return FutureBuildegr(
       builder: (context,snapshot){
           if(snapshot.hasData){
             return NewsListView(articles: snapshot.data!);
